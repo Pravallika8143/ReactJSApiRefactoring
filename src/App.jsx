0f02ea6336d8ddde44f5d96a4048e7401ab7b4f0
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-19 12:22:19 +0530

    ReactJSApiRefactoring

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Products from "./Products"
+
+function App() {
+  return (
+    <>
+    <div className="border border-5 p-2 m-2 border-dark">
+    <h1>Products</h1>
+    <Products></Products>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Products.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './Products.css'
+
+function Products(){
+    var[products,setProducts]=React.useState([])
+   
+    fetch("https://fakestoreapi.com/products")
+    .then((res)=>res.json())
+    .then ((data)=>{
+        setProducts([...data])
+    });
+   return (
+    <div className="border border-5 p-2 m-2 border-info">
+        <h1>Products Length:{products.length}</h1>
+        <ul className="products-list">
+            {
+                products.map((product)=>{
+                    return <li>
+                        <img src={product.image} style={{width:"200px",height:"200px"}} alt=""/>
+                        <h4>{product.title.slice(0,20)}</h4>
+                        <div>
+                            {product.rating.rate} 
+                            <i class="bi bi-star-fill"></i><br/>
+                            <i>Rs.{product.price}</i>
+                        </div>
+                        </li>
+                })
+            }
+        </ul>
+    </div>
+   )
+}
+export default Products;
