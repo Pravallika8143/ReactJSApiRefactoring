2f3ae8035fc3e560462be37ab6be04a29160dc4b
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-19 12:33:45 +0530

    useeffecthook

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Products.css'
 
 function Products(){
     var[products,setProducts]=React.useState([])
-   
+    useEffect(function (){
     fetch("https://fakestoreapi.com/products")
     .then((res)=>res.json())
     .then ((data)=>{
         setProducts([...data])
-    })
+    });
+   },[]);
    return (
     <div className="border border-5 p-2 m-2 border-info">
         <h1>Products Length:{products.length}</h1>
